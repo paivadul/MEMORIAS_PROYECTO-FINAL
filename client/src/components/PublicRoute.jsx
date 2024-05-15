89d6012c0d7be4cb1a2796ecdcd6345888538c1f
paivadul <dul.paiva.comunicaciones@gmail.com>
2024-05-14 23:43:15 -0400

    REORGANIZACIÓN DE ARCHIVOS: conflictos resueltos

--- client/src/components/PublicRoute.jsx
+++ client/src/components/PublicRoute.jsx
@@ -3,13 +3,6 @@ import { Navigate } from "react-router-dom";
 import { AppContext } from "../context/AppProvider";
 
 export const PublicRoute = ({ children }) => {
-<<<<<<< HEAD
-  const { state } = useContext(AppContext);
-
-  return !state.isAuthenticated ? children : <Navigate to="/inicio" />;
-};
-=======
     const token = localStorage.getItem('userToken');
     return token ? <Navigate to="/inicio" /> : children;
 }
->>>>>>> 4b1e4e4effa1dc54fa7b86a41c408de5a2350cc1
--- client/src/pages/inicio/Inicio.jsx
+++ client/src/pages/inicio/Inicio.jsx
@@ -12,14 +12,10 @@ export const Inicio = () => {
 
     return (
         <>
-<<<<<<< HEAD:client/src/pages/inicio/Inicio.jsx
-<h1>inicio</h1>
-=======
             <h1> Inicio
 
             </h1>
 
->>>>>>> 4b1e4e4effa1dc54fa7b86a41c408de5a2350cc1:client/src/pages/inicio.jsx
         </>
     )
 }
