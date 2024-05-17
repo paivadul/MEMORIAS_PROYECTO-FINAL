9d95a99b3ccead0e241ee86d7fe07d3ed4e5ca1f
Nahuel Cabral <151670293+bearcito@users.noreply.github.com>
2024-05-17 01:34:36 -0400

    no se guardan los cambiosssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- client/src/pages/usuario/Usuario.js
+++ client/src/pages/usuario/Usuario.js
@@ -1,9 +1,8 @@
-import React from "react";
 import React, { useContext, useEffect } from 'react';
 import { GlobalContext } from "../../context/GlobalContext";
 
 export const Usuario = () => {
-    const { user} = useContext(GlobalContext);
+    const { user } = useContext(GlobalContext);
 
     useEffect(() => {
     }, [user])
