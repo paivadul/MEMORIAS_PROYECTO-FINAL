95a28d14d7ab2bb2229121f18d5612ae2a7f4974
Nahuel Cabral <151670293+bearcito@users.noreply.github.com>
2024-05-17 03:26:16 -0400

    home setImages(response.data.anecdotas);

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,18 +1,38 @@
-import React, { useContext, useEffect } from 'react';
-import { GlobalContext } from '../../context/GlobalContext';
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './Home.css';
+import axios from 'axios';
 
 export const Home = () => {
-    const { user, setUser } = useContext(GlobalContext);
+    const [images, setImages] = useState([]);
 
     useEffect(() => {
-    }, [user])
+        // Simular una llamada al backend para obtener las imágenes
+        const fetchImages = async () => {
+            // Reemplaza esta URL con la llamada a tu backend
+            const response = await axios.get('http://localhost:8060/api/anecdota/all');
+            console.log(response);
+            setImages(response.data.anecdotas);
+        };
+
+        fetchImages();
+    }, []);
 
     return (
-        <>
-            <h1> Inicio
+        <div id="home-component">
+            <div id="home-carousel-container">
+                <Carousel>
+                    {images ?? images?.map((image, index) => (
+                        <div key={index} className="home-carousel-slide">
+                            <img src={image.url} alt={image.alt} className="home-carousel-image" />
+                        </div>
+                    ))}
+                </Carousel>
+            </div>
 
-            </h1>
 
-        </>
-    )
-}
+        </div>
+
+    );
+};
